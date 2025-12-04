0860ed70962ecae93c5480e0be4b29df22d0ff6c
soham518 <167617833+soham518@users.noreply.github.com>
2025-12-04 11:14:45 +0530

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+
 dotenv.config({path: "./.env"})
 
 //1. app setup
 const port = process.env.PORT;
 console.log(port)
 const app = express();
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.listen(port,()=>{
     console.log('app is running on port',port);
@@ -20,6 +24,15 @@ app.get('/',(req,res) => {
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
+
+//make redy for deployment
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+}
+
+app.get('*',(req,res)=> {
+    res.sendFile(path.join(__dirname,"../client","dist","index.html"));
+})
 //2.connect db
 mongoose.connect(process.env.MONGO_URI,{
     dbName: 'chatify'
